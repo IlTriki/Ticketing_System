884fa039f0d19d9f3b15b52caa87cf240f7f6646
Triki Osama <trikiosama@gmail.com>
2023-12-01 08:16:45 +0100

    barre de navigation + positionnement des elements

--- partie_mobile/lib/pages/home.dart
+++ partie_mobile/lib/pages/home.dart
@@ -11,7 +11,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late double screenWidth;
-  late double screenHeight;
   List<ProgrammeModel> programmes = [];
 
   @override
@@ -22,15 +21,15 @@ class _HomePageState extends State<HomePage> {
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    screenWidth = MediaQuery.of(context).size.width;
-    screenHeight = MediaQuery.of(context).size.height;
     programmes = ProgrammeModel.getProgrammes();
+    screenWidth = MediaQuery.of(context).size.width;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF5F5F5),
+      appBar: _appBar(),
       body: Column(
         children: [
           const SizedBox(
@@ -39,6 +38,23 @@ class _HomePageState extends State<HomePage> {
           _programmes(),
         ],
       ),
+      bottomNavigationBar: _bottomNavigationBar(),
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      title: Text(
+        'Programme du jour',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: screenWidth * 0.04,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
     );
   }
 
@@ -46,45 +62,34 @@ class _HomePageState extends State<HomePage> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Padding(
-          padding: EdgeInsets.only(
-            left: screenWidth * 0.05,
-            top: screenWidth * 0.05,
-          ),
-          child: Text(
-            'Programme du jour :',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: screenWidth * 0.04,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-        ),
         SizedBox(
-          height: screenHeight * 0.8,
+          height: MediaQuery.of(context).size.height * 0.78,
           child: ListView.separated(
-            itemCount: programmes.length,
-            padding: EdgeInsets.only(
-              left: screenWidth * 0.05,
-              right: screenWidth * 0.05,
-              bottom: screenHeight * 0.03,
-              top: screenHeight * 0.03,
-            ),
-            separatorBuilder: (context, index) => const SizedBox(
-              height: 25,
-            ),
-            itemBuilder: (context, index) {
-              return Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(screenWidth * 0.03),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              itemCount: programmes.length,
+              padding: const EdgeInsets.only(
+                bottom: 30,
+                right: 30,
+                left: 30,
+              ),
+              separatorBuilder: (context, index) => const SizedBox(
+                    height: 25,
+                  ),
+              itemBuilder: (context, index) {
+                return Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                        ),
+                      ],
+                    ),
+                    child: Stack(children: [
+                      Row(
                         children: [
                           SvgPicture.asset(
                             programmes[index].priorite,
@@ -99,51 +104,79 @@ class _HomePageState extends State<HomePage> {
                               color: const Color(0xFF8F9BB3),
                               fontSize: screenWidth * 0.035,
                             ),
-                          ),
-                          SvgPicture.asset('assets/icons/options.svg',
-                              width: screenWidth * 0.010,
-                              height: screenWidth * 0.010),
+                          )
                         ],
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              programmes[index].nom,
-                              style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                color: Colors.black,
-                                fontSize: screenWidth * 0.04,
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: SvgPicture.asset('assets/icons/options.svg',
+                            width: screenWidth * 0.010,
+                            height: screenWidth * 0.010),
+                      ),
+                      Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                programmes[index].nom,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black,
+                                  fontSize: screenWidth * 0.04,
+                                ),
                               ),
-                            ),
-                            Text(
-                              programmes[index].lieu,
-                              style: TextStyle(
-                                fontWeight: FontWeight.w400,
-                                color: const Color(0xFF8F9BB3),
-                                fontSize: screenWidth * 0.035,
+                              Text(
+                                programmes[index].lieu,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  color: const Color(0xFF8F9BB3),
+                                  fontSize: screenWidth * 0.035,
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                        SvgPicture.asset(
-                          'assets/icons/boutonGo.svg',
-                          width: screenWidth * 0.35,
-                          height: screenWidth * 0.35,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
+                            ],
+                          ),
+                          const Spacer(),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              SvgPicture.asset('assets/icons/boutonGo.svg',
+                                  width: screenWidth * 0.35)
+                            ],
+                          )
+                        ],
+                      )
+                    ]));
+              }),
         ),
       ],
     );
+  }
+
+  BottomNavigationBar _bottomNavigationBar() {
+    return BottomNavigationBar(backgroundColor: Colors.white, items: const [
+      BottomNavigationBarItem(
+        icon: Icon(
+          Icons.home,
+          color: Color(0xFF1A73E8),
+        ),
+        label: 'Accueil',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(
+          Icons.calendar_today,
+          color: Colors.black,
+        ),
+        label: 'Programme',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(
+          Icons.person,
+          color: Colors.black,
+        ),
+        label: 'Profil',
+      ),
+    ]);
   }
 }
