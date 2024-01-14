db46d620bc4d5bccc3ee593a09eceebd02fde99a
Triki Osama <trikiosama@gmail.com>
2024-01-14 17:46:17 +0100

    opti code

--- partie_mobile/lib/models/navigator_function.dart
+++ partie_mobile/lib/models/navigator_function.dart
@@ -4,8 +4,3 @@ void navigateToPage(BuildContext context, Widget page) {
   Navigator.pop(context);
   Navigator.push(context, MaterialPageRoute(builder: (context) => page));
 }
-
-void navigateToPageWithReplacement(BuildContext context, Widget page) {
-  Navigator.pushReplacement(
-      context, MaterialPageRoute(builder: (context) => page));
-}
